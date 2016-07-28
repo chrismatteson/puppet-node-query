8a0318084c7d881d8245bfeb5bcf498beda4f8dc
Chris Matteson <chris.matteson@puppet.com>
2016-07-28 19:05:40 +0000

    updates to allow script to work on any system and inclusion of readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# puppet-node-query
+# Puppet Node Query Tool
+
+This is a simple script which takes the fqdn of a server and returns
+the information concerning that node.  In order for the script to work
+the node which is running the query must be added to the auth.conf on
+the master server.  This can be done by finding the authentication line
+in /etc/puppetlabs/puppet/auth.conf (for puppetserver <2.2.0) or
+/etc/puppetlabs/puppetserver/conf.d/auth.conf (for puppetserver >2.2.0)
+for /puppet/v3/node([^/]+)$ and amending the allow line to inlude an
+array.  For >2.2.0 this looks like:
+            "allow" : ["$1","centos6a.pdx.puppet.vm"],
--- bin/node_query.sh
+++ bin/node_query.sh
@@ -5,16 +5,17 @@ PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
 declare -x PE_CERT=$(puppet agent --configprint hostcert)
 declare -x PE_KEY=$(puppet agent --configprint hostprivkey)
 declare -x PE_CA=$(puppet agent --configprint localcacert)
+declare -x PE_SERVER=$(puppet agent --configprint server)
 
 declare -x NC_CURL_OPT="-s --cacert $PE_CA --cert $PE_CERT --key $PE_KEY --insecure"
 
 show_node()
 {
-  curl $NC_CURL_OPT --insecure https://master.inf.puppet.vm:8140/puppet/v3/node/$1?environment=production | python -m json.tool
+  curl $NC_CURL_OPT --insecure https://$2:8140/puppet/v3/node/$1?environment=production | python -m json.tool
 }
 
 if [ "$1" == "" ]; then
   echo "This tool requires an fqdn as input"
 else
-  show_node $1
+  show_node $1 $PE_SERVER
 fi
